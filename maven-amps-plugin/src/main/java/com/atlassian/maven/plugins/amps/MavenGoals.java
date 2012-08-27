31807514588b562aa467c3a7329aad30e8d192e9
Jonathan Doklovic <doklovic@atlassian.com>
2012-08-27 09:43:15 -0700

    just testing if surefire upgrade broke bamboo build

--- maven-amps-plugin/src/main/java/com/atlassian/maven/plugins/amps/MavenGoals.java
+++ maven-amps-plugin/src/main/java/com/atlassian/maven/plugins/amps/MavenGoals.java
@@ -82,7 +82,8 @@ public class MavenGoals
             put("maven-dependency-plugin", "2.0");
             put("maven-resources-plugin", "2.3");
             put("maven-jar-plugin", "2.2");
-            put("maven-surefire-plugin", "2.12");
+            put("maven-surefire-plugin", "2.4.3");
+            //put("maven-surefire-plugin", "2.12");
 
         }};
 
