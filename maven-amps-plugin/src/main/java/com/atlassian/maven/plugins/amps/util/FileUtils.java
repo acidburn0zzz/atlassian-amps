1cd4678fbfaa3fd704281815fb92ad801fff7789
Charles O'Farrell <cofarrell@atlassian.com>
2012-12-18 12:58:46 +1100

    AMPS-904 Preserve executable status of home directory when copied

--- maven-amps-plugin/src/main/java/com/atlassian/maven/plugins/amps/util/FileUtils.java
+++ maven-amps-plugin/src/main/java/com/atlassian/maven/plugins/amps/util/FileUtils.java
@@ -3,6 +3,9 @@ package com.atlassian.maven.plugins.amps.util;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
+import java.util.List;
+
+import static org.apache.commons.io.FileUtils.copyFile;
 
 public class FileUtils
 {
@@ -69,6 +72,45 @@ public class FileUtils
 
         if (null != exception) {
             throw exception;
+        }
+    }
+
+    /**
+     * Copy all files and directories from one folder to another, preserving structure.
+     * This is an unfortunate duplicate of other libraries, which do not preserve the executable status of files,
+     * most likely due to Java version restrictions.
+     * <p />
+     * If you do <i>not</i> have this requirement please use commons-io instead:
+     * {@link org.apache.commons.io.FileUtils#copyDirectory(java.io.File, java.io.File)}.
+     * @param source source directory from which to copy all files/directories from
+     * @param destination destination directory to copy all files/directories to
+     * @param preserveFileAttrs indicate whether date and exec should be preserved
+     * @throws IOException if the destination files could not be created
+     */
+    public static void copyDirectory(File source, File destination, boolean preserveFileAttrs) throws IOException
+    {
+        if (!destination.mkdirs() && !destination.isDirectory())
+        {
+            throw new IOException("Destination '" + destination + "' directory cannot be created");
+        }
+        File[] srcFiles = source.listFiles();
+        // Who decided that listFiles could return null?!?
+        srcFiles = srcFiles != null ? srcFiles : new File[0];
+        for (File srcFile : srcFiles)
+        {
+            File dstFile = new File(destination, srcFile.getName());
+            if (srcFile.isDirectory())
+            {
+                copyDirectory(srcFile, dstFile, preserveFileAttrs);
+            }
+            else
+            {
+                copyFile(srcFile, dstFile, preserveFileAttrs);
+                if (preserveFileAttrs)
+                {
+                    dstFile.setExecutable(srcFile.canExecute());
+                }
+            }
         }
     }
 
--- maven-amps-plugin/src/test/java/com/atlassian/maven/plugins/amps/util/TestFileUtils.java
+++ maven-amps-plugin/src/test/java/com/atlassian/maven/plugins/amps/util/TestFileUtils.java
@@ -1,10 +1,12 @@
 package com.atlassian.maven.plugins.amps.util;
 
+import static com.atlassian.maven.plugins.amps.util.FileUtils.copyDirectory;
 import static com.atlassian.maven.plugins.amps.util.FileUtils.doesFileNameMatchArtifact;
 import static com.atlassian.maven.plugins.amps.util.FileUtils.file;
 import junit.framework.TestCase;
 
 import java.io.File;
+import java.io.IOException;
 
 public class TestFileUtils extends TestCase
 {
@@ -22,4 +24,31 @@ public class TestFileUtils extends TestCase
         assertTrue(doesFileNameMatchArtifact("sal-crowd-plugin-2.0.7.jar", "sal-crowd-plugin"));
         assertFalse(doesFileNameMatchArtifact("sal-crowd-plugin-2.0.7.jar", "crowd-plugin"));
     }
+
+    public void testCopyDirectory() throws IOException
+    {
+        File src = new File("src");
+        File dest = new File("dest");
+        try
+        {
+            src.mkdirs();
+            File file = new File(src, "something");
+            file.createNewFile();
+            file.setExecutable(true);
+            new File(src, "a/b").mkdirs();
+            new File(src, "a/b/c").createNewFile();
+            new File(src, "a/d").createNewFile();
+            copyDirectory(src, dest, true);
+            assertTrue(new File(dest, "a/b/c").exists());
+            assertTrue(new File(dest, "a/d").exists());
+            assertFalse(new File(dest, "a/d").canExecute());
+            assertTrue(new File(dest, file.getName()).canExecute());
+        }
+        finally
+        {
+            FileUtils.deleteDir(src);
+            FileUtils.deleteDir(dest);
+        }
+    }
+
 }
