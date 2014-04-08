c129a2b9264a214fb4ecd5a95a1a18c1056412db
Charles O'Farrell <cofarrell@atlassian.com>
2014-04-09 07:46:39 +1000

    PTRUNNER-23 Upgrade osgi-testrunner to support webresource 3.x

--- maven-amps-plugin/src/main/java/com/atlassian/maven/plugins/amps/AbstractProductHandlerMojo.java
+++ maven-amps-plugin/src/main/java/com/atlassian/maven/plugins/amps/AbstractProductHandlerMojo.java
@@ -49,7 +49,7 @@ public abstract class AbstractProductHandlerMojo extends AbstractProductHandlerA
     // ------ start inline product context
 
     protected static final String JUNIT_VERSION = "4.10_1";
-    protected static final String ATLASSIAN_TEST_RUNNER_VERSION = "1.1.4";
+    protected static final String ATLASSIAN_TEST_RUNNER_VERSION = "1.2.0";
     protected static final String NO_TEST_GROUP = "__no_test_group__";
     public static final String JUNIT_GROUP_ID = "org.apache.servicemix.bundles";
     public static final String JUNIT_ARTIFACT_ID = "org.apache.servicemix.bundles.junit";
