5aafd86994c5e4498ca2120f729db4efdf9b05d2
Marcos Scriven <mscriven@atlassian.com>
2014-04-24 19:47:44 +1000

    Increase the number of retries. Move catch out of for loop

--- maven-amps-plugin/src/it/debug/setup.groovy
+++ maven-amps-plugin/src/it/debug/setup.groovy
@@ -1,5 +1,5 @@
-MAX_RETRIES=6
-WAIT_PERIOD_IN_SECONDS=5
+MAX_RETRIES=20
+WAIT_PERIOD_IN_SECONDS=30
 DEFAULT_DEBUG_PORT=5005
 
 debugPort = Integer.getInteger('jvm.debug.port', DEFAULT_DEBUG_PORT);
@@ -7,21 +7,19 @@ println "Ensuring debug port:${debugPort} is closed"
 
 // We need to ensure that the debug port has closed from previous Tomcat instances
 // So we repeatedly try to open the port until we fail
-
 debugPortClosed = false;
-for(int attempt=0; attempt<MAX_RETRIES; attempt++)
-{
-    try
+
+try {
+    for(int attempt=0; attempt<MAX_RETRIES; attempt++)
     {
         socket = new Socket('localhost', debugPort);
         println "Debug port is still open - waiting for ${WAIT_PERIOD_IN_SECONDS} seconds";
         sleep WAIT_PERIOD_IN_SECONDS * 1000;
     }
-    catch (final IOException e) {
-        println "Port has closed";
-        debugPortClosed = true;
-        break;
-    }
+}
+catch (final IOException e) {
+    println "Port has closed";
+    debugPortClosed = true;
 }
 
 assert debugPortClosed, "Debug port ${debugPort} should be closed before commencing test"
