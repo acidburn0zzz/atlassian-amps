4b38eaff15105ddc396d5f33c2c852ceb03e6773
Marcos Scriven <mscriven@atlassian.com>
2014-04-28 08:04:15 +1000

    Reformat - no functional changes

--- maven-amps-plugin/src/it/debugWithTestGroup/setup.groovy
+++ maven-amps-plugin/src/it/debugWithTestGroup/setup.groovy
@@ -1,6 +1,6 @@
-MAX_RETRIES=20
-WAIT_PERIOD_IN_SECONDS=30
-DEFAULT_DEBUG_PORT=5005
+MAX_RETRIES = 20
+WAIT_PERIOD_IN_SECONDS = 30
+DEFAULT_DEBUG_PORT = 5005
 
 debugPort = Integer.getInteger('jvm.debug.port', DEFAULT_DEBUG_PORT);
 println "Ensuring debug port:${debugPort} is closed"
@@ -9,15 +9,17 @@ println "Ensuring debug port:${debugPort} is closed"
 // So we repeatedly try to open the port until we fail
 debugPortClosed = false;
 
-try {
-    for(int attempt=0; attempt<MAX_RETRIES; attempt++)
+try
+{
+    for (int attempt = 0; attempt < MAX_RETRIES; attempt++)
     {
         new Socket('localhost', debugPort);
         println "Debug port is still open - waiting for ${WAIT_PERIOD_IN_SECONDS} seconds";
         sleep WAIT_PERIOD_IN_SECONDS * 1000;
     }
 }
-catch (final IOException e) {
+catch (final IOException e)
+{
     println "Port has closed";
     debugPortClosed = true;
 }
