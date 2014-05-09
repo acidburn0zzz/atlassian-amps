dd7285aa1ec682c4860aa39ac78485baf325c7af
Marcos Scriven <mscriven@atlassian.com>
2014-05-09 14:28:09 +1000

    Set path to AMPS relative to the script location, so it can be run anywhere

--- atlassian-plugin-sdk/add_built_amps_to_path.sh
+++ atlassian-plugin-sdk/add_built_amps_to_path.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-# Source this script to use the locally built AMPS scripts for testing
-PATH="$(pwd)/target/bin:$PATH"
+# Source this script to use the locally built AMPS scripts for testing'
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PATH="$DIR/target/bin:$PATH"
