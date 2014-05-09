9f7beff182e5476334ccfe64ced3a3536e028c53
Marcos Scriven <mscriven@atlassian.com>
2014-05-09 14:30:57 +1000

    Softlink bundled Maven version for local testing without doing a full SDK release

--- atlassian-plugin-sdk/add_built_amps_to_path.sh
+++ atlassian-plugin-sdk/add_built_amps_to_path.sh
@@ -2,4 +2,6 @@
 
 # Source this script to use the locally built AMPS scripts for testing'
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ln -s $DIR/target/apache-maven-3.2.1 $DIR/target/apache-maven
 PATH="$DIR/target/bin:$PATH"
