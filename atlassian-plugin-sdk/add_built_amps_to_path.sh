55b77aacea39e7836b12109b05ca76bdf710871d
Marcos Scriven <mscriven@atlassian.com>
2014-05-13 09:06:23 +1000

    Update Maven version mentioned in comment, and remove a spurious single quote in a bash script comment

--- atlassian-plugin-sdk/add_built_amps_to_path.sh
+++ atlassian-plugin-sdk/add_built_amps_to_path.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Source this script to use the locally built AMPS scripts for testing'
+# Source this script to use the locally built AMPS scripts for testing
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ln -s $DIR/target/apache-maven-3.2.1 $DIR/target/apache-maven
