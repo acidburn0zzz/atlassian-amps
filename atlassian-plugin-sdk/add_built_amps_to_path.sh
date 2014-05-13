b6aa0d90774777980d9bf29301e9cfad580dfb42
Marcos Scriven <mscriven@atlassian.com>
2014-05-13 11:15:28 +1000

    Drop shebang, and quote  in case of spaces

--- atlassian-plugin-sdk/add_built_amps_to_path.sh
+++ atlassian-plugin-sdk/add_built_amps_to_path.sh
@@ -1,8 +1,6 @@
-#!/bin/sh
-
 # Source this script to use the locally built AMPS scripts for testing
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ln -s $DIR/target/apache-maven-3.2.1 $DIR/target/apache-maven
-chmod u+x $DIR/target/bin/*
+ln -s "$DIR/target/apache-maven-3.2.1" "$DIR/target/apache-maven"
+chmod u+x "$DIR"/target/bin/*
 PATH="$DIR/target/bin:$PATH"
