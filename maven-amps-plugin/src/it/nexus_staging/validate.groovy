f34535a043d3c03c0c7baf15eecaf2b090c59796
Milos Kleint <mkleint@atlassian.com>
2014-10-30 20:45:41 +1100

    AMPS-1199 more descriptive assert messages because i'm clueless.

--- maven-amps-plugin/src/it/nexus_staging/validate.groovy
+++ maven-amps-plugin/src/it/nexus_staging/validate.groovy
@@ -1,16 +1,16 @@
 assert mavenExitCode == 0, "The maven build should not have failed!"
 
 def stagingrepo = new File(basedir, 'target/releases')
-assert stagingrepo.exists()
+assert stagingrepo.exists(), "staging repo should exist at:" + stagingrepo
 
 //we assert that the deploy plugin repo is not used. That would happen if if
 // defined a custom goal for deploying again and reuse the deploy plugins config as we did with mvn-deploy
 // in normal case nexus staging should override that.
 def deployrepo = new File(basedir, 'target/releases-wrong')
-assert !deployrepo.exists()
+assert !deployrepo.exists(), "deploy repo should not exist at:" + deployrepo
 
 def deferred = new File(basedir, "target/nexus-staging/deferred/.index");
-assert stagingrepo.exists()
+assert deferred.exists(), "deffered directory should exist at:" + deffered
 //we want to make sure that nexus staging .index properties file only contains
 // one entry per file. With attached jar artifact we ended up with 2 and the wrong one took over because it
 // came later in the file.
