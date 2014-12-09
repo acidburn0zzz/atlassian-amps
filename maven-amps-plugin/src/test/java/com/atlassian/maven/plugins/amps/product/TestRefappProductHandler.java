e2012ecab8a3a564b9e20b21944fd97ea74cf635
Tom Nguyen <dnguyentan@atlassian.com>
2014-12-09 12:03:33 +0700

    REFAPP-372: Do not bundle the bundled plugins in a zip     Update after review: use org.junit.rules.TemporaryFolder

--- maven-amps-plugin/src/test/java/com/atlassian/maven/plugins/amps/product/TestRefappProductHandler.java
+++ maven-amps-plugin/src/test/java/com/atlassian/maven/plugins/amps/product/TestRefappProductHandler.java
@@ -2,69 +2,37 @@ package com.atlassian.maven.plugins.amps.product;
 
 import com.atlassian.maven.plugins.amps.MavenContext;
 import com.atlassian.maven.plugins.amps.Product;
-import org.apache.commons.io.FileUtils;
 import org.apache.maven.plugin.logging.Log;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.UUID;
 
-import static com.atlassian.maven.plugins.amps.product.RefappProductHandler.ATLASSIAN_BUNDLED_PLUGINS_DIR;
 import static com.atlassian.maven.plugins.amps.product.RefappProductHandler.ATLASSIAN_BUNDLED_PLUGINS_ZIP;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class TestRefappProductHandler
 {
-    private File tempHome;
-
-    @Before
-    public void createTemporaryHomeDirectory() throws IOException
-    {
-        final File f = File.createTempFile("temp-refapp-home-" + UUID.randomUUID(), null);
-        if (!f.delete())
-        {
-            throw new IOException();
-        }
-
-        if (!f.mkdir())
-        {
-            throw new IOException();
-        }
-
-        tempHome = f;
-    }
-
-    @After
-    public void deleteTemporaryHomeDirectoryAndContents() throws Exception
-    {
-        if (tempHome != null)
-        {
-            FileUtils.deleteDirectory(tempHome);
-            tempHome = null;
-        }
-    }
+    @Rule
+    public TemporaryFolder tempHome = new TemporaryFolder();
 
     @Test
     public void bundledPluginsLocationCorrectForDirectory()
     {
-        final File bundledPluginsDir = new File(tempHome, ATLASSIAN_BUNDLED_PLUGINS_DIR);
-        assertThat(bundledPluginsDir.mkdirs(), is(true));
-
-        assertBundledPluginPath(tempHome, bundledPluginsDir);
+        final File bundledPluginsDir = tempHome.newFolder(ATLASSIAN_BUNDLED_PLUGINS_ZIP);
+        assertBundledPluginPath(tempHome.getRoot(), bundledPluginsDir);
     }
 
     @Test
     public void bundledPluginsLocationCorrectForFallback()
     {
-        final File bundledPluginsDir = new File(tempHome, ATLASSIAN_BUNDLED_PLUGINS_ZIP);
-
-        assertBundledPluginPath(tempHome, bundledPluginsDir);
+        final File bundledPluginsDir = new File(tempHome.getRoot(), ATLASSIAN_BUNDLED_PLUGINS_ZIP);
+        assertBundledPluginPath(tempHome.getRoot(), bundledPluginsDir);
     }
 
     private void assertBundledPluginPath(final File appDir, final File expectedPath)
