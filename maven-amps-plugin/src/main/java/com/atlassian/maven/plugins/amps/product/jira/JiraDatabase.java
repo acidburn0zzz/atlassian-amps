370059d5976af290f1d39f3c2700cddf049e716b
Lucas Le <llephamngoc@atlassian.com>
2014-12-15 03:38:42 +0700

    AMPSDEV-55: add javadoc for JiraDatabase interface

--- maven-amps-plugin/src/main/java/com/atlassian/maven/plugins/amps/product/jira/JiraDatabase.java
+++ maven-amps-plugin/src/main/java/com/atlassian/maven/plugins/amps/product/jira/JiraDatabase.java
@@ -1,21 +1,21 @@
 package com.atlassian.maven.plugins.amps.product.jira;
 
-import com.atlassian.maven.plugins.amps.DataSource;
-
 import org.apache.maven.model.Dependency;
 import org.codehaus.plexus.util.xml.Xpp3Dom;
 
 public interface JiraDatabase
 {
     /**
-     *
-     * @return
+     * create sql-maven-plugin configuration, include all sql for drop/create database
+     * Please refer to documentation of maven-sql-plugin at url
+     * http://mojo.codehaus.org/sql-maven-plugin/index.html
+     * @return configuration
      */
     Xpp3Dom getPluginConfiguration();
 
     /**
-     *
-     * @return
+     * create database library dependency for sql-maven-plugin connect to database
+     * @return dependency
      */
     Dependency getDependency();
 }
