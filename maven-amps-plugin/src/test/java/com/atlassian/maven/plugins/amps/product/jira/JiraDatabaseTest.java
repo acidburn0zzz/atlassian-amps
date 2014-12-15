d8a7b3b22b2b9c0dbd3078fb883efe6fbb79dc68
Lucas Le <llephamngoc@atlassian.com>
2014-12-15 10:36:11 +0700

    AMPSDEV-55: add unit test for MSSQL, MYSQL get database name function

--- maven-amps-plugin/src/test/java/com/atlassian/maven/plugins/amps/product/jira/JiraDatabaseTest.java
+++ maven-amps-plugin/src/test/java/com/atlassian/maven/plugins/amps/product/jira/JiraDatabaseTest.java
@@ -1,39 +1,41 @@
 package com.atlassian.maven.plugins.amps.product.jira;
 
 import com.atlassian.maven.plugins.amps.DataSource;
-import com.atlassian.maven.plugins.amps.LibArtifact;
-
-import com.google.common.collect.ImmutableList;
 
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mock;
 
 public class JiraDatabaseTest
 {
     @Test
     public void postgresDatabaseName() throws Exception
     {
-        final DataSource dataSource = postgresDataSourceTest();
+        final DataSource dataSource = mock(DataSource.class);
         final JiraDatabasePostgresqlImpl postgres = new JiraDatabasePostgresqlImpl(dataSource);
         assertThat("database name should be : ddd", postgres.getDatabaseName("abc:ddd"), equalTo("ddd"));
+        assertThat("database name should be : eeee", postgres.getDatabaseName("jdbc:postgresql://host/eeee"), equalTo("eeee"));
+        assertThat("database name should be : fff", postgres.getDatabaseName("jdbc:postgresql://host:port/fff"), equalTo("fff"));
     }
 
-    private static final DataSource postgresDataSourceTest()
+    @Test
+    public void mysqlDatabaseName() throws Exception
     {
-        final DataSource dataSource = new DataSource();
-        dataSource.setDriver("");
-        dataSource.setUrl("");
-        dataSource.setUsername("");
-        dataSource.setPassword("");
+        final DataSource dataSource = mock(DataSource.class);
+        final JiraDatabaseMysqlImpl mysql = new JiraDatabaseMysqlImpl(dataSource);
+        assertThat("database name should be : ddd", mysql.getDatabaseName("jdbc:mysql://localhost:3306/ddd"), equalTo("ddd"));
+        assertThat("database name should be : eeee", mysql.getDatabaseName("jdbc:mysql://127.0.0.1:3306/eeee?profileSQL=true"), equalTo("eeee"));
+    }
 
-        final LibArtifact lib = new LibArtifact();
-        lib.setGroupId("");
-        lib.setArtifactId("");
-        lib.setVersion("");
-        dataSource.setLibArtifacts(ImmutableList.of(lib));
-        return dataSource;
+    @Test
+    public void mssqlDatabaseName() throws Exception
+    {
+        final DataSource dataSource = mock(DataSource.class);
+        final JiraDatabaseMssqlImpl mssql = new JiraDatabaseMssqlImpl(dataSource);
+        assertThat("database name should be : ddd", mssql.getDatabaseName("jdbc:jtds:sqlserver://localhost:1433/ddd"), equalTo("ddd"));
+        assertThat("database name should be : eeee", mssql.getDatabaseName("jdbc:jtds:sybase://127.0.0.1/eeee;autoCommit=false"), equalTo("eeee"));
     }
 
 }
