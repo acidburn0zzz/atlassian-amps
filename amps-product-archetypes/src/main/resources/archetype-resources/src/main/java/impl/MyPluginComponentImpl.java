d7d8923a2203893b249445485a799cce78437e91
Tom Nguyen <dnguyentan@atlassian.com>
2014-12-31 12:13:05 +0700

    AMPSDEV-58: atlas-create-*-plugin archetypes should create version 3 plugin (transformerless) - Use spring scanner, correct pack imports.

--- amps-product-archetypes/src/main/resources/archetype-resources/src/main/java/impl/MyPluginComponentImpl.java
+++ amps-product-archetypes/src/main/resources/archetype-resources/src/main/java/impl/MyPluginComponentImpl.java
@@ -1,16 +1,20 @@
 package ${package}.impl;
 
+import com.atlassian.plugin.spring.scanner.annotation.export.ExportAsService;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 import com.atlassian.sal.api.ApplicationProperties;
 import ${package}.api.MyPluginComponent;
 
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@ExportAsService ({MyPluginComponent.class})
+@Named ("myPluginComponent")
 public class MyPluginComponentImpl implements MyPluginComponent
 {
-    private final ApplicationProperties applicationProperties;
-
-    public MyPluginComponentImpl(ApplicationProperties applicationProperties)
-    {
-        this.applicationProperties = applicationProperties;
-    }
+    @ComponentImport
+    @Inject
+    private ApplicationProperties applicationProperties;
 
     public String getName()
     {
