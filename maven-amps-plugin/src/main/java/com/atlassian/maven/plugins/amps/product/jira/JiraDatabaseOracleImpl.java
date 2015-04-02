4c48c2c95eefbbe8a0b54557ce784a37c6323e50
Lucas Le <llephamngoc@atlassian.com>
2015-04-02 13:47:33 +0700

    Oracle: improve drop tablespace if exists

--- maven-amps-plugin/src/main/java/com/atlassian/maven/plugins/amps/product/jira/JiraDatabaseOracleImpl.java
+++ maven-amps-plugin/src/main/java/com/atlassian/maven/plugins/amps/product/jira/JiraDatabaseOracleImpl.java
@@ -28,6 +28,13 @@ public class JiraDatabaseOracleImpl extends AbstractJiraDatabase
                     + "    THEN\n"
                     + "        EXECUTE IMMEDIATE('DROP USER %s CASCADE');\n"
                     + "    END IF;\n"
+                    + "    v_count := 0; \n"
+                    + "    SELECT COUNT (1) INTO v_count FROM dba_tablespaces WHERE tablespace_name = UPPER('jiradb2'); \n"
+                    + "    IF v_count != 0\n"
+                    + "    THEN\n"
+                    + "        EXECUTE IMMEDIATE('DROP TABLESPACE jiradb2 INCLUDING CONTENTS');\n"
+                    + "    END IF;\n"
+
                     + "    EXECUTE IMMEDIATE(q'{CREATE TABLESPACE jiradb2 DATAFILE 'jiradb2.dbf' SIZE 32m AUTOEXTEND ON NEXT 32m MAXSIZE 4096m EXTENT MANAGEMENT LOCAL}');\n"
                     + "    EXECUTE IMMEDIATE('CREATE USER %s IDENTIFIED BY %s DEFAULT TABLESPACE jiradb2 QUOTA UNLIMITED ON jiradb2');\n"
                     + "    EXECUTE IMMEDIATE('GRANT CONNECT, RESOURCE, IMP_FULL_DATABASE TO %s');\n"
@@ -71,7 +78,9 @@ public class JiraDatabaseOracleImpl extends AbstractJiraDatabase
         final String dumpFileDirectoryPath = (new File(getDataSource().getDumpFilePath())).getParent();
         final String username = getDataSource().getUsername();
         final String dropAndCreateUser = String.format(DROP_AND_CREATE_USER,
+                // drop user if exists
                 username, username,
+                // create user with default tablespace
                 username, getDataSource().getPassword(), username
                 , DATA_PUMP_DIR, dumpFileDirectoryPath,
                 DATA_PUMP_DIR, username
