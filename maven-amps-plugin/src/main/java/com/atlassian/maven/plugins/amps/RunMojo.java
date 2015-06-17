1013cf79ff37fbbb66b90f8e3fb8fce576093a55
Lucas Le <llephamngoc@atlassian.com>
2015-06-17 17:05:20 +0700

    Abstract getProductExecutions function

--- maven-amps-plugin/src/main/java/com/atlassian/maven/plugins/amps/RunMojo.java
+++ maven-amps-plugin/src/main/java/com/atlassian/maven/plugins/amps/RunMojo.java
@@ -48,11 +48,6 @@ public class RunMojo extends AbstractTestGroupsHandlerMojo
     @Parameter(property = "amps.properties", required = true, defaultValue = "false")
     protected boolean writePropertiesToFile;
 
-    /**
-     * Test group to run. If provided, used to determine the products to run.
-     */
-    @Parameter(property = "testGroup")
-    protected String testGroup;
 
     /**
      * When this property is set to {@literal true}, Mojo will be executed on the last project in the Reactor
@@ -226,75 +221,6 @@ public class RunMojo extends AbstractTestGroupsHandlerMojo
             // application running after the end of the RunMojo goal. The SHITTY tests
             // check this behaviour.
             stopProducts(productExecutions);
-        }
-    }
-
-    protected List<ProductExecution> getProductExecutions() throws MojoExecutionException
-    {
-        final List<ProductExecution> productExecutions;
-        final MavenGoals goals = getMavenGoals();
-        if (!isBlank(testGroup))
-        {
-            productExecutions = getTestGroupProductExecutions(testGroup);
-        }
-        else if (!isBlank(instanceId))
-        {
-            Product ctx = getProductContexts().get(instanceId);
-            if (ctx == null)
-            {
-                throw new MojoExecutionException("No product with instance ID '" + instanceId + "'");
-            }
-            ProductHandler product = createProductHandler(ctx.getId());
-            productExecutions = Collections.singletonList(new ProductExecution(ctx, product));
-        }
-        else
-        {
-            Product ctx = getProductContexts().get(getProductId());
-            ProductHandler product = createProductHandler(ctx.getId());
-            productExecutions = Collections.singletonList(new ProductExecution(ctx, product));
-        }
-        return filterExcludedInstances(includeStudioDependentProducts(productExecutions, goals));
-    }
-
-    private List<ProductExecution> filterExcludedInstances(List<ProductExecution> executions) throws MojoExecutionException
-    {
-        if (StringUtils.isBlank(excludeInstances))
-        {
-            return executions;
-        }
-        boolean inverted = excludeInstances.startsWith("*/");
-        String instanceIdList = inverted ? excludeInstances.substring(2) : excludeInstances;
-
-        // Parse the list given by the user and find ProductExecutions
-        List<String> excludedInstanceIds = Lists.newArrayList(instanceIdList.split(","));
-        List<ProductExecution> excludedExecutions = Lists.newArrayList();
-        for (final String instanceId : excludedInstanceIds)
-        {
-            try
-            {
-                excludedExecutions.add(Iterables.find(executions, new Predicate<ProductExecution>()
-                {
-                    @Override
-                    public boolean apply(ProductExecution input)
-                    {
-                        return input.getProduct().getInstanceId().equals(instanceId);
-                    }
-                }));
-            }
-            catch (NoSuchElementException nsee)
-            {
-                throw new MojoExecutionException("You specified -Dexclude=" + excludeInstances + " but " + instanceId + " is not an existing instance id.");
-            }
-        }
-
-        if (inverted)
-        {
-            return excludedExecutions;
-        }
-        else
-        {
-            executions.removeAll(excludedExecutions);
-            return executions;
         }
     }
 
