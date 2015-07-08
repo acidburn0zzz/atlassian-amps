d4b81b4f5a0f871cc2584f1ea848627b797f76dc
Elena Vasilyeva <evasilyeva@atlassian.com>
2015-07-08 14:56:16 +1000

    brackets style

--- amps-product-plugins/maven-fecru-plugin/src/main/java/com/atlassian/maven/plugins/fecru/FeCruCreateMojo.java
+++ amps-product-plugins/maven-fecru-plugin/src/main/java/com/atlassian/maven/plugins/fecru/FeCruCreateMojo.java
@@ -9,12 +9,14 @@ import org.apache.maven.plugins.annotations.Mojo;
 @Mojo(name = "create", requiresProject = false)
 public class FeCruCreateMojo extends CreateMojo {
     @Override
-    protected String getDefaultProductId() throws MojoExecutionException {
+    protected String getDefaultProductId() throws MojoExecutionException
+    {
         return ProductHandlerFactory.FECRU;
     }
 
     @Override
-    protected void doExecute() throws MojoExecutionException, MojoFailureException {
+    protected void doExecute() throws MojoExecutionException, MojoFailureException
+    {
         //due to AMPS-1260
         getLog().error("!!! Important information !!!");
         getLog().error("FeCru does not support java8 yet so this command is temporarily switched off. Try using an earlier amps version or creating a default amps plugin instead: atlas-create-plugin");
