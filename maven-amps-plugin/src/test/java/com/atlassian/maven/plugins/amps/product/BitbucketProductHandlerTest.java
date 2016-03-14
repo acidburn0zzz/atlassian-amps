5efff79c80c9a9d87f1aa3de61936a89688bec8e
Kristy Hughes <khughes@atlassian.com>
2016-03-15 10:01:08 +1100

    BSERVDEV-9463 Update tests to ensure the correct version is extracted from the dependencies

--- maven-amps-plugin/src/test/java/com/atlassian/maven/plugins/amps/product/BitbucketProductHandlerTest.java
+++ maven-amps-plugin/src/test/java/com/atlassian/maven/plugins/amps/product/BitbucketProductHandlerTest.java
@@ -4,17 +4,27 @@ import com.atlassian.maven.plugins.amps.MavenContext;
 import com.atlassian.maven.plugins.amps.MavenGoals;
 import com.atlassian.maven.plugins.amps.Product;
 import com.atlassian.maven.plugins.amps.ProductArtifact;
+import com.atlassian.maven.plugins.amps.util.ArtifactRetriever;
+import com.atlassian.maven.plugins.amps.util.MavenProjectLoader;
+import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.factory.ArtifactFactory;
+import org.apache.maven.model.Dependency;
+import org.apache.maven.model.DependencyManagement;
+import org.apache.maven.project.MavenProject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyBoolean;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -33,29 +43,47 @@ public class BitbucketProductHandlerTest {
     private ArtifactFactory artifactFactory;
 
     private BitbucketProductHandler bitbucketProductHandler;
+    private static final String SEARCH_VERSION = "1.0.0";
+    private static final String SEARCH_GROUP_ID = "com.atlassian.bitbucket.search";
+    private static final String SEARCH_ARTIFACT_ID = "search-plugin";
+    private static final String EMBEDDED_ARTIFACT_ID = "embedded-elasticsearch-plugin";
+    private static final ProductArtifact embeddedBundle = new ProductArtifact(SEARCH_GROUP_ID, EMBEDDED_ARTIFACT_ID, SEARCH_VERSION);
 
     @Before
-    public void setUp() {
+    public void setUp() throws Exception {
         this.bitbucketProductHandler = new BitbucketProductHandler(mavenContext, mavenGoals, artifactFactory);
+
+        ArtifactRetriever artifactRetriever = mock(ArtifactRetriever.class);
+        MavenProjectLoader mavenProjectLoader = mock(MavenProjectLoader.class);
+        MavenProject mavenProject = mock(MavenProject.class);
+        DependencyManagement dependencyManagement = mock(DependencyManagement.class);
+        Dependency searchDependency = mock(Dependency.class);
+
+        when(ctx.getArtifactRetriever()).thenReturn(artifactRetriever);
+        when(artifactRetriever.getMavenProjectLoader(mavenContext)).thenReturn(mavenProjectLoader);
+        when(mavenProjectLoader.loadMavenProject(any(Artifact.class), anyBoolean(), anyBoolean())).thenReturn(mavenProject);
+        when(mavenProject.getDependencyManagement()).thenReturn(dependencyManagement);
+        when(dependencyManagement.getDependencies()).thenReturn(Collections.singletonList(searchDependency));
+        when(searchDependency.getGroupId()).thenReturn(SEARCH_GROUP_ID);
+        when(searchDependency.getArtifactId()).thenReturn(SEARCH_ARTIFACT_ID);
+        when(searchDependency.getVersion()).thenReturn(SEARCH_VERSION);
     }
 
-    private boolean pluginFoundInPluginList(List<ProductArtifact> pluginList, ProductArtifact searchBundle) {
-        return pluginList.stream().anyMatch(p -> p.equals(searchBundle));
+    private boolean pluginFoundInPluginList(List<ProductArtifact> pluginList) {
+        return pluginList.stream().anyMatch(p -> p.equals(embeddedBundle));
     }
 
     @Test
     public void testGetAdditionalPluginsBeforeFirstSearchVersion() throws Exception {
         when(ctx.getVersion()).thenReturn("4.4.0");
-        ProductArtifact searchBundle = new ProductArtifact("com.atlassian.bitbucket.server", "bitbucket-distribution-search-bundle", ctx.getVersion());
         List<ProductArtifact> pluginList = bitbucketProductHandler.getAdditionalPlugins(ctx);
-        assertFalse(pluginFoundInPluginList(pluginList, searchBundle));
+        assertFalse(pluginFoundInPluginList(pluginList));
     }
 
     @Test
     public void testGetAdditionalPluginsAfterFirstSearchVersion() throws Exception {
         when(ctx.getVersion()).thenReturn("4.6.0");
-        ProductArtifact searchBundle = new ProductArtifact("com.atlassian.bitbucket.server", "bitbucket-distribution-search-bundle", ctx.getVersion());
         List<ProductArtifact> pluginList = bitbucketProductHandler.getAdditionalPlugins(ctx);
-        assertTrue(pluginFoundInPluginList(pluginList, searchBundle));
+        assertTrue(pluginFoundInPluginList(pluginList));
     }
 }
