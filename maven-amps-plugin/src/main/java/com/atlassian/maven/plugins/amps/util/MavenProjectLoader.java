d1c4469f4035b22d948addac425f40e78384e1d6
Kristy Hughes <khughes@atlassian.com>
2016-06-28 10:18:44 +1000

    Bugfix: Update MavenProjectLoader to get the projectBuildingRequest from the session directly instead of the session's request

--- maven-amps-plugin/src/main/java/com/atlassian/maven/plugins/amps/util/MavenProjectLoader.java
+++ maven-amps-plugin/src/main/java/com/atlassian/maven/plugins/amps/util/MavenProjectLoader.java
@@ -1,7 +1,6 @@
 package com.atlassian.maven.plugins.amps.util;
 
 import org.apache.maven.artifact.Artifact;
-import org.apache.maven.execution.MavenExecutionRequest;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.model.building.ModelBuildingRequest;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -19,29 +18,30 @@ public class MavenProjectLoader
      * Resolve a given artifact with its dependencies
      * @param session
      * @param pomArtifact
-     * @param incudeDependencies
+     * @param includeDependencies
      * @return  Optional.of(MavenProject) if the maven project for the given artifact and session can be resolved.
      *          Optional.empty() otherwise
      */
-    public Optional<MavenProject> loadMavenProject(MavenSession session, Artifact pomArtifact, boolean incudeDependencies)
+    public Optional<MavenProject> loadMavenProject(MavenSession session, Artifact pomArtifact, boolean includeDependencies)
             throws MojoExecutionException
     {
-        MavenExecutionRequest executionRequest = session.getRequest();
         try
         {
             ProjectBuilder projectBuilder = session.getContainer().lookup(ProjectBuilder.class);
-            ProjectBuildingRequest projectBuildingRequest = executionRequest.getProjectBuildingRequest()
+            ProjectBuildingRequest projectBuildingRequest = session.getProjectBuildingRequest()
                     // The validation level is currently "VALIDATION_LEVEL_MAVEN_3_0".
                     // Setting it to minimal might speed things up a bit
                     .setValidationLevel(ModelBuildingRequest.VALIDATION_LEVEL_MINIMAL)
                     // ResolveDependencies is currently "false".
                     // If the calling function needs dependencies, we need to tell maven about it
-                    .setResolveDependencies(incudeDependencies);
+                    .setResolveDependencies(includeDependencies);
             return Optional.ofNullable(projectBuilder.build(pomArtifact, projectBuildingRequest).getProject());
-        } catch (ComponentLookupException e)
+        }
+        catch (ComponentLookupException e)
         {
             throw new MojoExecutionException("Could not get the ProjectBuilder from the maven session", e);
-        } catch (ProjectBuildingException e)
+        }
+        catch (ProjectBuildingException e)
         {
             throw new MojoExecutionException(String.format("Could not build the MavenProject for %s", pomArtifact), e);
         }
