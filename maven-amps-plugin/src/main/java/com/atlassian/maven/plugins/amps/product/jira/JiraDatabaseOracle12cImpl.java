c6a082b9ccc8477e2683c713cfdf62f971329230
Andrew Swan <aswan@atlassian.com>
2016-09-22 13:53:02 +1000

    AMPS-1363 Using moved readFileToString method

--- maven-amps-plugin/src/main/java/com/atlassian/maven/plugins/amps/product/jira/JiraDatabaseOracle12cImpl.java
+++ maven-amps-plugin/src/main/java/com/atlassian/maven/plugins/amps/product/jira/JiraDatabaseOracle12cImpl.java
@@ -1,18 +1,14 @@
 package com.atlassian.maven.plugins.amps.product.jira;
 
 import com.atlassian.maven.plugins.amps.DataSource;
-import org.apache.commons.io.IOUtils;
+import com.atlassian.maven.plugins.amps.util.FileUtils;
 
 import javax.annotation.Nonnull;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Map;
 
-import static java.lang.String.format;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.Collections.singletonMap;
-import static java.util.Objects.requireNonNull;
 
 public class JiraDatabaseOracle12cImpl extends AbstractJiraOracleDatabase
 {
@@ -25,20 +21,10 @@ public class JiraDatabaseOracle12cImpl extends AbstractJiraOracleDatabase
     {
         // This path is only valid if Oracle can read the local filesystem, e.g. is not running in a VM
         final String dumpFileDirectoryPath = (new File(getDataSource().getDumpFilePath())).getParent();
-        return readFileToString("oracle12c-template.sql")
+        return FileUtils.readFileToString("oracle12c-template.sql", getClass(), UTF_8)
                 .replace("v_data_pump_dir", dumpFileDirectoryPath)
                 .replace("v_jira_user", getDataSource().getUsername())
                 .replace("v_jira_pwd", getDataSource().getPassword());
-    }
-
-    private String readFileToString(final String name) {
-        final InputStream fileStream = getClass().getResourceAsStream(name);
-        requireNonNull(fileStream, format("Could not find '%s' on classpath of %s", name, getClass().getName()));
-        try {
-            return IOUtils.toString(fileStream, UTF_8);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     @Nonnull
