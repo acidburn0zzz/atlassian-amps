e02598b24121f4747c979dc5e7b3eafca63463fb
Tony Gong <tgong@atlassian.com>
2016-12-16 14:26:04 +1100

    Default plugin versions are now taken from application-versions.xml

--- maven-amps-plugin/src/main/java/com/atlassian/maven/plugins/amps/CreateMojo.java
+++ maven-amps-plugin/src/main/java/com/atlassian/maven/plugins/amps/CreateMojo.java
@@ -12,6 +12,14 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Mojo;
 
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.XMLEvent;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 /**
  * Creates a new plugin
  */
@@ -29,29 +37,96 @@ public class CreateMojo extends AbstractProductHandlerMojo
         getGoogleTracker().track(GoogleAmpsTracker.CREATE_PLUGIN);
 
 
+        // this is the name of a product (refapp, jira, confluence, etc)
         String pid = getProductId();
-        Product ctx = getProductContexts().get(pid);
-        AbstractProductHandler handler = createProductHandler(pid);
+        String stableVersion, stableDataVersion;
 
-        getLog().info("determining latest stable product version...");
-        String stableVersion = getStableProductVersion(handler,ctx);
+        // first try to get manual version
+        Application a = getManualVersion(pid);
+        if (a != null) {
+            getLog().info("using manually-entered latest stable product version: " + a.latest);
+            getLog().info("using manually-entered latest stable data version: " + a.data);
+            stableVersion = a.latest;
+            stableDataVersion = a.data;
+        } else {
+            // use the old way (grab version from artifact)
+            Product ctx = getProductContexts().get(pid);
+            AbstractProductHandler handler = createProductHandler(pid);
+            getLog().info("determining latest stable product version...");
+            stableVersion = getStableProductVersion(handler,ctx);
+            if (StringUtils.isNotBlank(stableVersion))
+            {
+                getLog().info("using latest stable product version: " + stableVersion);
+            }
 
-        if (StringUtils.isNotBlank(stableVersion))
-        {
-            getLog().info("using latest stable product version: " + stableVersion);
-            getMavenContext().getExecutionEnvironment().getMavenSession().getExecutionProperties().setProperty(pid + "Version", stableVersion);
+            getLog().info("determining latest stable data version...");
+            stableDataVersion = getStableDataVersion(handler,ctx);
+            if (StringUtils.isNotBlank(stableDataVersion))
+            {
+                getLog().info("using latest stable data version: " + stableDataVersion);
+            }
         }
+        getMavenContext().getExecutionEnvironment().getMavenSession().getExecutionProperties().setProperty(pid + "Version", stableVersion);
+        getMavenContext().getExecutionEnvironment().getMavenSession().getExecutionProperties().setProperty(pid + "DataVersion", stableDataVersion);
 
-        getLog().info("determining latest stable data version...");
-        String stableDataVersion = getStableDataVersion(handler,ctx);
-
-        if (StringUtils.isNotBlank(stableDataVersion))
-        {
-            getLog().info("using latest stable data version: " + stableDataVersion);
-            getMavenContext().getExecutionEnvironment().getMavenSession().getExecutionProperties().setProperty(pid + "DataVersion", stableDataVersion);
-        }
 
         getMavenGoals().createPlugin(getProductId(), ampsCreatePluginPrompter);
+    }
+
+    /**
+     * @param searchString  The name of a product (refapp, jira, confluence, etc)
+     * @return  An Application object holding latest stable/data version strings
+     *          and some other bits of information.
+     */
+    private Application getManualVersion(String searchString) {
+
+        XMLInputFactory f = XMLInputFactory.newInstance();
+        try {
+            XMLEventReader r = f.createXMLEventReader(CreateMojo.class.getClassLoader().getResourceAsStream("application-versions.xml"));
+            Application a = new Application();
+            String name = "";
+            while (r.hasNext()) {
+                XMLEvent e = r.nextEvent();
+                switch (e.getEventType()) {
+                    case XMLStreamConstants.START_ELEMENT:
+                        name = e.asStartElement().getName().getLocalPart();
+                        if (name.equalsIgnoreCase("application"))
+                            a = new Application();
+                        break;
+                    case XMLStreamConstants.END_ELEMENT:
+                        if (e.asEndElement().getName().getLocalPart().equalsIgnoreCase("application") &&
+                                a.name.equals(searchString))
+                            return a;
+                        name = "";
+                        break;
+                    case XMLStreamConstants.CHARACTERS:
+                        String s = e.asCharacters().getData().trim();
+                        if (name.equalsIgnoreCase("name"))
+                            a.name = s;
+                        else if (name.equalsIgnoreCase("latest"))
+                            a.latest = s;
+                        else if (name.equalsIgnoreCase("data"))
+                            a.data = s;
+                        else if (name.equalsIgnoreCase("mvn-artifact"))
+                            a.mvnArtifact = s;
+                        break;
+                }
+            }
+        } catch (XMLStreamException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static class Application {
+        public String name;
+        public String latest;
+        public String data;
+        public String mvnArtifact;
+
+        public String toString() {
+            return "name=" + name + " latest=" + latest + " data=" + data + " mvn-artifact=" + mvnArtifact;
+        }
     }
 
     protected String getStableProductVersion(AbstractProductHandler handler, Product ctx) throws MojoExecutionException
