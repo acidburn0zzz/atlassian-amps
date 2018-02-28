917348d7a40b66162fb93d9a89451205359c3c49
Joe Harris <jharris@atlassian.com>
2018-02-28 13:52:45 +1100

    specify imports and fix url to packages.atlassian.com

--- maven-amps-plugin/src/main/java/com/atlassian/maven/plugins/amps/RunStandaloneMojo.java
+++ maven-amps-plugin/src/main/java/com/atlassian/maven/plugins/amps/RunStandaloneMojo.java
@@ -18,7 +18,10 @@ import org.apache.maven.project.ProjectBuildingRequest;
 import org.apache.maven.project.ProjectBuildingResult;
 import org.codehaus.plexus.util.xml.Xpp3Dom;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.List;
@@ -115,7 +118,7 @@ public class RunStandaloneMojo extends AbstractProductHandlerMojo
             } else {
                 getLog().error("There is no valid full version of "+productVersion+". Please double check your input");
                 getLog().error("The full list of versions can be found at: " +
-                        "https://maven.atlassian.com/content/repositories/atlassian-public/com/atlassian/fecru/amps-fecru/");
+                        "https://packages.atlassian.com/content/repositories/atlassian-public/com/atlassian/fecru/amps-fecru/");
             }
             getLog().error("=======================================================================");
             System.exit(1);
@@ -149,18 +152,17 @@ public class RunStandaloneMojo extends AbstractProductHandlerMojo
     }
 
     private String getFullVersion(String versionInput) {
-        URLConnection connection = null;
+        URLConnection connection;
         String correctVersion = null;
         BufferedReader in = null;
         Matcher m;
         try {
-            connection = new URL("https://maven.atlassian.com/content/repositories/atlassian-public/com/atlassian/fecru/amps-fecru/").openConnection();
+            connection = new URL("https://packages.atlassian.com/content/repositories/atlassian-public/com/atlassian/fecru/amps-fecru/").openConnection();
             in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
         } catch (IOException e) {
             e.printStackTrace();
         }
         try {
-//            response = connection.getInputStream();
             String inputLine;
             // Read each line of the page and parse it to see the version number
             while ((inputLine = in.readLine()) != null) {
