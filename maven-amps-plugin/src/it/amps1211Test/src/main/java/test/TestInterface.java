8b32c956baa28adcdb249d0797e1834bcb6666a1
Pawel Drygas <pdrygas@atlassian.com>
2019-01-10 10:00:25 +0100

    Revert "AMPS-1454: add a couple of @see links"
    This reverts commit fde884c0f860fb7aaca61374367fd71b80834f95.

--- maven-amps-plugin/src/it/amps1211Test/src/main/java/test/TestInterface.java
+++ maven-amps-plugin/src/it/amps1211Test/src/main/java/test/TestInterface.java
@@ -7,9 +7,6 @@ package test;
  * The third operand byte (13h) is misinterpreted by the affected version as the start of a new
  * <code>ldc_w</code> instruction, where the fourth operand byte (00h) and the next actual opcode
  * (B1h) form an <code>#index</code> (00B1h) beyond the constant pool of this small class.
- *
- * @see <a href="https://issues.apache.org/jira/browse/FELIX-4556">FELIX-4556</a>
- * @see <a href="https://github.com/bndtools/bnd/issues/603">bndtools/bnd#603</a>
  */
 interface TestInterface {
     void method(
