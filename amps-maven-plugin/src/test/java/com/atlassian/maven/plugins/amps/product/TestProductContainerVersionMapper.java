decafc995a2de8b9b3850baca71b91a8fb9814a8
Pawel Drygas <pdrygas@atlassian.com>
2019-05-10 11:03:32 +0200

    RAID-1243 There are no plans to release Jira 8.1.2, Tomcat upgrade should then land in the next bugfix release which is Jira 8.2.1

--- amps-maven-plugin/src/test/java/com/atlassian/maven/plugins/amps/product/TestProductContainerVersionMapper.java
+++ amps-maven-plugin/src/test/java/com/atlassian/maven/plugins/amps/product/TestProductContainerVersionMapper.java
@@ -44,7 +44,7 @@ public class TestProductContainerVersionMapper
         assertThat(ProductContainerVersionMapper.containerForProductVersion("jira", "7.6.0"), is("tomcat85_29"));
         assertThat(ProductContainerVersionMapper.containerForProductVersion("jira", "7.12.3"), is("tomcat85_32"));
         assertThat(ProductContainerVersionMapper.containerForProductVersion("jira", "7.13.2"), is("tomcat85_35"));
-        assertThat(ProductContainerVersionMapper.containerForProductVersion("jira", "8.1.2"), is("tomcat85x"));
+        assertThat(ProductContainerVersionMapper.containerForProductVersion("jira", "8.2.1"), is("tomcat85x"));
     }
 
     @Test
