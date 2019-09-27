8576506acb97d4035f356aa7cf9b8a0f11866d45
Pawel Przytarski <pprzytarski@atlassian.com>
2019-09-27 12:23:33 +0200

    NONE: Add integration tests for new functionality

---
+++ amps-maven-plugin/src/it/integrationTestWithOverlappingCustomContainers/validate.groovy
@@ -0,0 +1,12 @@
+def product = binding.product
+product = 'amps'.equals(product) ? 'refapp' : product
+
+def logFile = new File(basedir, "target/jira/home/log/atlassian-jira.log")
+assert logFile.exists(), "Integration tests should have run and created test reports in $logFile"
+
+logFile.eachLine {
+    if (it.contains('Application Server')) {
+        println it
+        assert it.contains('Apache Tomcat/8.5.42')
+    }
+}
