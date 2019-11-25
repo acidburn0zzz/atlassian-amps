a6638cda82499ddb23cf0c27507db70fe8227493
Mark Rekveld <markrekveld@marvelution.com>
2019-11-25 12:06:34 +0100

    Corrected to quickly applied feedback

--- amps-maven-plugin/src/main/java/com/atlassian/maven/plugins/amps/product/AmpsProductHandler.java
+++ amps-maven-plugin/src/main/java/com/atlassian/maven/plugins/amps/product/AmpsProductHandler.java
@@ -198,7 +198,7 @@ public abstract class AmpsProductHandler implements ProductHandler
         if (isNotBlank(overridesPath))
         {
             srcDir = new File(overridesPath);
-            if (srcDir.isDirectory())
+            if (!srcDir.exists() || !srcDir.isDirectory())
             {
                 srcDir = new File(project.getBasedir(), overridesPath);
             }
