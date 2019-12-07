a850edb986b52e8fa226b7a020c84bec167be662
Christopher Darroch <cdarroch@atlassian.com>
2019-12-07 10:18:58 +0100

    AMPS-1511: add basic test coverage for the ResourcesMinifier class.

--- amps-maven-plugin/src/test/java/com/atlassian/maven/plugins/amps/minifier/ResourcesMinifierTest.java
+++ amps-maven-plugin/src/test/java/com/atlassian/maven/plugins/amps/minifier/ResourcesMinifierTest.java
@@ -3,6 +3,7 @@ package com.atlassian.maven.plugins.amps.minifier;
 import org.apache.commons.compress.utils.Lists;
 import org.apache.maven.model.Resource;
 import org.apache.maven.plugin.logging.Log;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
@@ -15,6 +16,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsCollectionContaining.hasItems;
 import static org.hamcrest.core.StringEndsWith.endsWith;
@@ -22,13 +24,55 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class ResourcesMinifierTest {
+    private Log log;
+    private Path dir;
+    private Path out;
+
+
+    @Before
+    public void setUp() throws Exception {
+        log = mock(Log.class);
+        dir = Files.createTempDirectory("resources");
+        out = Files.createTempDirectory("out");
+    }
+
     @Test
-    public void testPreMinifiedFilesAreCopied() throws Exception {
-        Log log = mock(Log.class);
+    public void testCssGetsMinified() throws Exception {
         MinifierParameters params = new MinifierParameters(true, true, true, StandardCharsets.UTF_8, log, new HashMap<>());
 
-        Path dir = Files.createTempDirectory("resources");
-        Path out = Files.createTempDirectory("out");
+        Files.createTempFile(dir, "test", "myapp1.css");
+        Resource resource = mock(Resource.class);
+        when(resource.getDirectory()).thenReturn(dir.toString());
+        when(resource.getIncludes()).thenReturn(Lists.newArrayList());
+        when(resource.getExcludes()).thenReturn(Lists.newArrayList());
+
+        new ResourcesMinifier(params).minify(Collections.singletonList(resource), out.toString());
+
+        List<String> results = Arrays.stream(out.toFile().listFiles()).map(File::getName).collect(Collectors.toList());
+        assertThat(results, hasItems(endsWith("myapp1-min.css")));
+        assertThat(results, not(hasItems(endsWith("myapp1.css"))));
+    }
+
+    @Test
+    public void testJsGetsMinified() throws Exception {
+        MinifierParameters params = new MinifierParameters(true, true, true, StandardCharsets.UTF_8, log, new HashMap<>());
+
+        Files.createTempFile(dir, "test", "myapp1.js");
+        Resource resource = mock(Resource.class);
+        when(resource.getDirectory()).thenReturn(dir.toString());
+        when(resource.getIncludes()).thenReturn(Lists.newArrayList());
+        when(resource.getExcludes()).thenReturn(Lists.newArrayList());
+
+        new ResourcesMinifier(params).minify(Collections.singletonList(resource), out.toString());
+
+        List<String> results = Arrays.stream(out.toFile().listFiles()).map(File::getName).collect(Collectors.toList());
+        assertThat(results, hasItems(endsWith("myapp1-min.js")));
+        assertThat(results, not(hasItems(endsWith("myapp1.js"))));
+    }
+
+    @Test
+    public void testPreMinifiedFilesAreCopied() throws Exception {
+        MinifierParameters params = new MinifierParameters(true, true, true, StandardCharsets.UTF_8, log, new HashMap<>());
 
         Files.createTempFile(dir, "test", "myapp1-min.css");
         Files.createTempFile(dir, "test", "myapp2.min.js");
