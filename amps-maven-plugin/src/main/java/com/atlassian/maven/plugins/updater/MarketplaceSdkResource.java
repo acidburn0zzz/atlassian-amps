2ef5d6e15ace7af072362af26a0468315221c343
Patryk Petrowski <ppetrowski@atlassian.com>
2020-04-10 15:41:33 +0200

    BSP-1192: bump Jackson to 2.10.3

--- amps-maven-plugin/src/main/java/com/atlassian/maven/plugins/updater/MarketplaceSdkResource.java
+++ amps-maven-plugin/src/main/java/com/atlassian/maven/plugins/updater/MarketplaceSdkResource.java
@@ -1,9 +1,9 @@
 package com.atlassian.maven.plugins.updater;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableMap;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.plexus.logging.AbstractLogEnabled;
 
 import java.io.BufferedInputStream;
